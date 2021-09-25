50de6faa005668c88bd0f97fd1ad27e342bfa264
Si Jine Roh <2108512+sjroh@users.noreply.github.com>
2021-09-25 14:48:00 +0800

    Trying different marketplace actions;

--- pythoning.py
+++ pythoning.py
@@ -5,8 +5,3 @@ print("Hello World from Python")
 if len(sys.argv) > 1:
     arg = sys.argv[1]
     print(f"Received arg: {arg}")
-
-# TODO: Print out message when there is no argument to process
-# 
-# Issue body;
-# Should print out as "There is no given argument(s)."
